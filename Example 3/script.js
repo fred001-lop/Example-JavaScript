dd15e1dce5e28a80807b7b542776744b8008fb5a
Fredy Lopez <fredygutierrez1661@gmail.com>
2026-01-03 18:43:36 -0600

    Finalizacion de example 3

---
+++ Example 3/script.js
@@ -0,0 +1,11 @@
+
+
+function openModal(modalId){
+    //show the overlay 
+    document.getElementById('overlay').style.display = "block";
+    document.getElementById("fact").style.display = "block";
+}
+
+function claseModal(){
+    document.getElementById("overlay").style.display = "none";
+}
